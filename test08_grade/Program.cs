19bc60dea2e51272204db1fab6b246f87abbf392
5506021621138 <5506021621138@fitm.kmutnb.ac.th>
2016-02-15 12:10:52 +0700

    สุ่มคะแนนเกรด
    สุ่มคะแนน ให้ 10 คน โดยแต่ละคนมี คะแนน ทั้งหมด 100
    แบ่งเป็น คะแนนเก็บ 15 คะแนน จิตพิสัย 5 คะแนน ทดสอบย่อย 15 คะแนน
    คะแนนสอบกลางภาค 30 ปลายภาค 35
    และคำนวณเกรดของแต่ละคน

---
+++ test08_grade/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Multi12
+ * Date: 8/2/2559
+ * Time: 10:21
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace test08_grade
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
